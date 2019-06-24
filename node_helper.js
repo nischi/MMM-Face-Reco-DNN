dc18ee392a92431cca3a236c1487b2aae1744514
Thierry Nischelwitzer <nischi@nischi.ch>
2019-06-24 21:14:19 +0200

    add some comments and send right values back to the magic mirror module

--- node_helper.js
+++ node_helper.js
@@ -13,25 +13,39 @@ var pythonStarted = false
 module.exports = NodeHelper.create({
   python_start: function () {
     const self = this;
+    // Start face reco script
     const pyshell = new PythonShell('modules/' + this.name + '/tools/facerecognition.py', {
       mode: 'json',
       args: [ JSON.stringify(this.config) ]
     });
 
+    // check if a message of the python script is comming in
     pyshell.on('message', function (message) {
+      // A status message has received and will log
       if (message.hasOwnProperty('status')){
         console.log("[" + self.name + "] " + message.status);
       }
+
+      // Somebody new are in front of the camera, send it back to the Magic Mirror Module
       if (message.hasOwnProperty('login')){
-        console.log("[" + self.name + "] " + "User " + self.config.users[message.login.user - 1] + " with confidence " + message.login.confidence + " logged in.");
-        self.sendSocketNotification('user', {action: "login", user: message.login.user - 1, confidence: message.login.confidence});
-        }
+        console.log("[" + self.name + "] " + "Users " + message.login.names.join(' - ') + " logged out.");
+        self.sendSocketNotification('user', {
+          action: "login",
+          users: message.login.names
+        });
+      }
+
+      // Somebody left the camera, send it back to the Magic Mirror Module
       if (message.hasOwnProperty('logout')){
-        console.log("[" + self.name + "] " + "User " + self.config.users[message.logout.user - 1] + " logged out.");
-        self.sendSocketNotification('user', {action: "logout", user: message.logout.user - 1});
-        }
+        console.log("[" + self.name + "] " + "Users " + message.logout.names.join(' - ') + " logged out.");
+        self.sendSocketNotification('user', {
+          action: "logout",
+          users: message.logout.names
+        });
+      }
     });
 
+    // Shutdown node helper
     pyshell.end(function (err) {
       if (err) throw err;
       console.log("[" + self.name + "] " + 'finished running...');
@@ -39,6 +53,7 @@ module.exports = NodeHelper.create({
   },
 
   socketNotificationReceived: function(notification, payload) {
+    // Configuration are received
     if(notification === 'CONFIG') {
       this.config = payload
       if(!pythonStarted) {

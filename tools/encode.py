edfd7d132ba805b01ed05cda3cb02055243e07ee
Thierry Nischelwitzer <nischi@nischi.ch>
2020-01-05 13:24:47 +0100

    write also file path into log

--- tools/encode.py
+++ tools/encode.py
@@ -30,8 +30,9 @@ knownNames = []
 # loop over the image paths
 for (i, imagePath) in enumerate(imagePaths):
 	# extract the person name from the image path
-	print("[INFO] processing image {}/{}".format(i + 1,
-		len(imagePaths)))
+	print("[INFO] processing image {}/{} - {}".format(i + 1,
+		len(imagePaths),
+		imagePath))
 	name = imagePath.split(os.path.sep)[-2]
 
 	# load the input image and convert it from RGB (OpenCV ordering)

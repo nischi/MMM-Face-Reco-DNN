e0599d8d2b5e76fc17db643b93f82188397f5bca
Thierry Nischelwitzer <nischi@nischi.ch>
2020-03-21 19:19:11 +0100

    add eslint and prettier

--- MMM-Face-Reco-DNN.js
+++ MMM-Face-Reco-DNN.js
@@ -7,19 +7,20 @@
 
 'use strict';
 
-Module.register("MMM-Face-Reco-DNN", {
-	defaults: {
+Module.register('MMM-Face-Reco-DNN', {
+  defaults: {
     // Logout 15 seconds after user was not detecte anymore, if they will be detected between this 15
     // Seconds, they delay will start again
     logoutDelay: 15000,
     // How many time the recognition starts, with a RasPi 3+ it would be good every 2 seconds
     checkInterval: 2000,
     // Module set used for strangers and if no user is detected
-    defaultClass: "default",
+    defaultClass: 'default',
     // Set of modules which should be shown for every user
-    everyoneClass: "everyone",
+    everyoneClass: 'everyone',
     // xml to recognize with haarcascae
-    cascade: 'modules/MMM-Face-Reco-DNN/tools/haarcascade_frontalface_default.xml',
+    cascade:
+      'modules/MMM-Face-Reco-DNN/tools/haarcascade_frontalface_default.xml',
     // pre encoded pickle with the faces
     encodings: 'modules/MMM-Face-Reco-DNN/tools/encodings.pickle',
     // You wanna use pi camera or usb / builtin (1 = raspi camera, 0 = other camera)
@@ -30,164 +31,182 @@ Module.register("MMM-Face-Reco-DNN", {
     // deep-learning model which is GPU/CUDA accelerated (if available). The default is "hog".
     detectionMethod: 'hog',
     // how fast in ms should the modules hide and show (face effect)
-		animationSpeed: 0,
-		// Path to Python to run the face recognition (null / '' means default path)
-		pythonPath: null,
-		// Boolean to toggle welcomeMessage
-		welcomeMessage: true,
-		// Save some pictures from recognized people, if unknown we save it in folder "unknown"
-		// So you can extend your dataset and retrain it afterwards for better recognitions
-		extendDataset: false,
-		// if extenDataset is set, you need to set the full path of the dataset
-		dataset: 'modules/MMM-Face-Reco-DNN/dataset/'
-	},
-
-	timouts: {},
-	users: [],
-
-	start: function() {
-		this.sendSocketNotification('CONFIG', this.config);
-    Log.log("Starting module: " + this.name);
-	},
-
-	// Define required translations.
-	getTranslations: function() {
-		return {
-			en: "translations/en.json",
-			de: "translations/de.json",
-      es: "translations/es.json",
-      zh: "translations/zh.json",
-      nl: "translations/nl.json",
-			sv: "translations/sv.json",
-			fr: "translations/fr.json",
-			id: "translations/id.json",
-			it: "translations/it.json",
-			bg: "translations/bg.json"
-		};
+    animationSpeed: 0,
+    // Path to Python to run the face recognition (null / '' means default path)
+    pythonPath: null,
+    // Boolean to toggle welcomeMessage
+    welcomeMessage: true,
+    // Save some pictures from recognized people, if unknown we save it in folder "unknown"
+    // So you can extend your dataset and retrain it afterwards for better recognitions
+    extendDataset: false,
+    // if extenDataset is set, you need to set the full path of the dataset
+    dataset: 'modules/MMM-Face-Reco-DNN/dataset/',
   },
 
-	login_user: function (name) {
+  timouts: {},
+  users: [],
+
+  start: function() {
+    this.sendSocketNotification('CONFIG', this.config);
+    Log.log('Starting module: ' + this.name);
+  },
+
+  // Define required translations.
+  getTranslations: function() {
+    return {
+      en: 'translations/en.json',
+      de: 'translations/de.json',
+      es: 'translations/es.json',
+      zh: 'translations/zh.json',
+      nl: 'translations/nl.json',
+      sv: 'translations/sv.json',
+      fr: 'translations/fr.json',
+      id: 'translations/id.json',
+      it: 'translations/it.json',
+      bg: 'translations/bg.json',
+    };
+  },
+
+  login_user: function(name) {
     var self = this;
 
     MM.getModules()
       .withClass(this.config.defaultClass)
       .exceptWithClass(this.config.everyoneClass)
       .enumerate(function(module) {
-				module.hide(self.config.animationSpeed, function() {
-					Log.log(module.name + ' is hidden.');
-				}, {
-					lockString: self.identifier
-				});
-			});
+        module.hide(
+          self.config.animationSpeed,
+          function() {
+            Log.log(module.name + ' is hidden.');
+          },
+          {
+            lockString: self.identifier,
+          }
+        );
+      });
 
-		MM.getModules()
-			.withClass(name.toLowerCase())
-			.enumerate(function(module) {
-				module.show(self.config.animationSpeed, function() {
-					Log.log(module.name + ' is shown.');
-				}, {
-					lockString: self.identifier
-				});
-			});
+    MM.getModules()
+      .withClass(name.toLowerCase())
+      .enumerate(function(module) {
+        module.show(
+          self.config.animationSpeed,
+          function() {
+            Log.log(module.name + ' is shown.');
+          },
+          {
+            lockString: self.identifier,
+          }
+        );
+      });
 
-		if (this.config.welcomeMessage) {
-			var person = name;
-			// We get unknown from Face-Reco and then it should be translated to stranger
-			if (person === 'unknown') {
-				person = this.translate('stranger');
-			}
+    if (this.config.welcomeMessage) {
+      var person = name;
+      // We get unknown from Face-Reco and then it should be translated to stranger
+      if (person === 'unknown') {
+        person = this.translate('stranger');
+      }
 
-			this.sendNotification("SHOW_ALERT", {
-				type: "notification",
-				message: this.translate("message").replace("%person", person),
-				title: this.translate("title")
-			});
-		}
+      this.sendNotification('SHOW_ALERT', {
+        type: 'notification',
+        message: this.translate('message').replace('%person', person),
+        title: this.translate('title'),
+      });
+    }
   },
 
-	logout_user: function (name) {
+  logout_user: function(name) {
     var self = this;
 
-		MM.getModules()
-			.withClass(name.toLowerCase())
-			.enumerate(function(module) {
-				module.hide(self.config.animationSpeed, function() {
-					Log.log(module.name + ' is hidden.');
+    MM.getModules()
+      .withClass(name.toLowerCase())
+      .enumerate(function(module) {
+        module.hide(
+          self.config.animationSpeed,
+          function() {
+            Log.log(module.name + ' is hidden.');
+          },
+          {
+            lockString: self.identifier,
+          }
+        );
+      });
 
-				}, {
-					lockString: self.identifier
-				});
-			});
+    if (this.users.length === 0) {
+      MM.getModules()
+        .withClass(self.config.defaultClass)
+        .exceptWithClass(self.config.everyoneClass)
+        .enumerate(function(module) {
+          module.show(
+            self.config.animationSpeed,
+            function() {
+              Log.log(module.name + ' is shown.');
+            },
+            {
+              lockString: self.identifier,
+            }
+          );
+        });
+    }
+  },
 
-		if (this.users.length === 0) {
-			MM.getModules()
-				.withClass(self.config.defaultClass)
-				.exceptWithClass(self.config.everyoneClass)
-				.enumerate(function(module) {
-					module.show(self.config.animationSpeed, function() {
-						Log.log(module.name + ' is shown.');
-					}, {
-						lockString: self.identifier
-					});
-				});
-		}
-	},
+  socketNotificationReceived: function(notification, payload) {
+    var self = this;
+    var user;
 
-	socketNotificationReceived: function(notification, payload) {
-		var self = this;
+    // somebody has logged in
+    if (payload.action === 'login') {
+      for (user of payload.users) {
+        if (user != null) {
+          this.users.push(user);
+          this.login_user(user);
 
-		// somebody has logged in
-		if (payload.action == "login") {
-			for (var user of payload.users) {
-				if (user != null) {
-					this.users.push(user);
-					this.login_user(user);
+          if (this.timouts[user] != null) {
+            clearTimeout(this.timouts[user]);
+          }
+        }
+      }
+      this.sendNotification('USERS_LOGIN', payload.users);
+    } else if (payload.action === 'logout') {
+      for (user of payload.users) {
+        if (user != null) {
+          this.timouts[user] = setTimeout(function() {
+            self.users = self.users.filter(function(u) {
+              return u !== user;
+            });
+            self.logout_user(user);
+          }, this.config.logoutDelay);
+        }
+      }
 
-					if (this.timouts[user] != null) {
-						clearTimeout(this.timouts[user]);
-					}
-				}
-			}
+      this.sendNotification('USERS_LOGOUT', payload.users);
+    }
+  },
 
-			this.sendNotification("USERS_LOGIN", payload.users);
-		}
-
-		// somebody has logged out
-		else if (payload.action == "logout") {
-			for (var user of payload.users) {
-				if (user != null) {
-					this.timouts[user] = setTimeout(function() {
-						self.users = self.users.filter(function(u) { return u !== user });
-						self.logout_user(user);
-					}, this.config.logoutDelay);
-				}
-			}
-
-			this.sendNotification("USERS_LOGOUT", payload.users);
-		}
-	},
-
-	notificationReceived: function(notification, payload, sender) {
-		var self = this;
+  notificationReceived: function(notification, payload, sender) {
+    var self = this;
 
     // Event if DOM is created
-		if (notification === 'DOM_OBJECTS_CREATED') {
+    if (notification === 'DOM_OBJECTS_CREATED') {
       // Show all Modules with default class
-			MM.getModules()
-				.exceptWithClass(this.config.defaultClass)
-				.enumerate(function(module) {
-					module.hide(0, function() {
-						Log.log('Module is hidden.');
-					}, {
-						lockString: self.identifier
-					});
-				});
-		}
+      MM.getModules()
+        .exceptWithClass(this.config.defaultClass)
+        .enumerate(function(module) {
+          module.hide(
+            0,
+            function() {
+              Log.log('Module is hidden.');
+            },
+            {
+              lockString: self.identifier,
+            }
+          );
+        });
+    }
 
-		// load logged in users
-		if (notification === 'GET_LOGGED_IN_USERS') {
-			Log.log(this.name + ' get logged in users ' + this.users);
-			this.sendNotification("LOGGED_IN_USERS", this.users);
-		}
-	}
+    // load logged in users
+    if (notification === 'GET_LOGGED_IN_USERS') {
+      Log.log(this.name + ' get logged in users ' + this.users);
+      this.sendNotification('LOGGED_IN_USERS', this.users);
+    }
+  },
 });
--- node_helper.js
+++ node_helper.js
@@ -7,18 +7,18 @@
 
 'use strict';
 const NodeHelper = require('node_helper');
-const {PythonShell} = require("python-shell");
-const onExit = require('signal-exit')
+const { PythonShell } = require('python-shell');
+const onExit = require('signal-exit');
 var pythonStarted = false;
-var pyshell;
 
 module.exports = NodeHelper.create({
-  python_start: function () {
+  pyshell: null,
+  python_start: function() {
     const self = this;
     const extendedDataset = this.config.extendDataset ? 'True' : 'False';
     const options = {
       mode: 'json',
-      stderrParser: (line) => JSON.stringify(line),
+      stderrParser: line => JSON.stringify(line),
       args: [
         '--cascade=' + this.config.cascade,
         '--encodings=' + this.config.encodings,
@@ -28,52 +28,69 @@ module.exports = NodeHelper.create({
         '--interval=' + this.config.checkInterval,
         '--output=' + this.config.output,
         '--extendDataset=' + extendedDataset,
-        '--dataset=' + this.config.dataset
-      ]
-    }
+        '--dataset=' + this.config.dataset,
+      ],
+    };
 
     if (this.config.pythonPath != null && this.config.pythonPath !== '') {
       options.pythonPath = this.config.pythonPath;
     }
 
     // Start face reco script
-    self.pyshell = new PythonShell('modules/' + this.name + '/tools/facerecognition.py', options);
+    self.pyshell = new PythonShell(
+      'modules/' + this.name + '/tools/facerecognition.py',
+      options
+    );
 
     // check if a message of the python script is comming in
-    self.pyshell.on('message', function (message) {
+    self.pyshell.on('message', function(message) {
       // A status message has received and will log
-      if (message.hasOwnProperty('status')){
-        console.log("[" + self.name + "] " + message.status);
+      if (message.hasOwnProperty('status')) {
+        console.log('[' + self.name + '] ' + message.status);
       }
 
       // Somebody new are in front of the camera, send it back to the Magic Mirror Module
-      if (message.hasOwnProperty('login')){
-        console.log("[" + self.name + "] " + "Users " + message.login.names.join(' - ') + " logged in.");
+      if (message.hasOwnProperty('login')) {
+        console.log(
+          '[' +
+            self.name +
+            '] ' +
+            'Users ' +
+            message.login.names.join(' - ') +
+            ' logged in.'
+        );
         self.sendSocketNotification('user', {
-          action: "login",
-          users: message.login.names
+          action: 'login',
+          users: message.login.names,
         });
       }
 
       // Somebody left the camera, send it back to the Magic Mirror Module
-      if (message.hasOwnProperty('logout')){
-        console.log("[" + self.name + "] " + "Users " + message.logout.names.join(' - ') + " logged out.");
+      if (message.hasOwnProperty('logout')) {
+        console.log(
+          '[' +
+            self.name +
+            '] ' +
+            'Users ' +
+            message.logout.names.join(' - ') +
+            ' logged out.'
+        );
         self.sendSocketNotification('user', {
-          action: "logout",
-          users: message.logout.names
+          action: 'logout',
+          users: message.logout.names,
         });
       }
     });
 
     // Shutdown node helper
-    self.pyshell.end(function (err) {
+    self.pyshell.end(function(err) {
       if (err) throw err;
-      console.log("[" + self.name + "] " + 'finished running...');
+      console.log('[' + self.name + '] ' + 'finished running...');
     });
 
-    onExit(function (code, signal) {
+    onExit(function(code, signal) {
       self.destroy();
-    })
+    });
   },
 
   python_stop: function() {
@@ -81,25 +98,25 @@ module.exports = NodeHelper.create({
   },
 
   destroy: function() {
-    console.log("[" + this.name + "] " + "Terminate python");
+    console.log('[' + this.name + '] ' + 'Terminate python');
     this.pyshell.childProcess.kill();
   },
 
   socketNotificationReceived: function(notification, payload) {
     // Configuration are received
-    if(notification === 'CONFIG') {
-      this.config = payload
+    if (notification === 'CONFIG') {
+      this.config = payload;
       // Set static output to 0, because we do not need any output for MMM
-      this.config['output'] = 0;
-      if(!pythonStarted) {
+      this.config.output = 0;
+      if (!pythonStarted) {
         pythonStarted = true;
         this.python_start();
-        };
-    };
+      }
+    }
   },
 
   stop: function() {
     pythonStarted = false;
     this.python_stop();
-  }
+  },
 });
